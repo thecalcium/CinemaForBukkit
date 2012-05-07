dd1fc79f44211a94173f5c24da2655729dcd92e1
Freddy <Freddy@192.168.2.113>
2012-05-07 20:39:40 +0200

    finally did not that what i thought... yay for code duplication!!

--- Cinema/src/com/forgewareinc/Cinema/Cinema.java
+++ Cinema/src/com/forgewareinc/Cinema/Cinema.java
@@ -28,7 +28,7 @@ public class Cinema extends JavaPlugin{
 	public static final String configPath = "plugins/cinema.cfg";
 	public static final String persistentPath = "plugins/cinemafile";
 	public static final String savePath = "plugins/cinema/";
-	public static final int version = 150;
+	public static final int version = 151;
 	public static int newestVersion = version;
 	
 	public boolean newVersionAvail(){
